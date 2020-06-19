2938f6aea09788b90de51bbf189905baad2a1581
Doni Setiawan <donisetiawan.050@gmail.com>
2020-06-19 15:41:15 +0700

    Preparing our project

---
+++ App.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import MainApp from './src/MainApp';
+
+export default function App() {
+  return <MainApp />;
+}
---
+++ src/MainApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+function MainApp() {
+  return <View style={styles.container} />;
+}
+
+export default MainApp;
